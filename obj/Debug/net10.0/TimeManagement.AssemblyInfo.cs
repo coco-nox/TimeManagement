96d28947030710a7e46995e985877583a1ec02c4
coco-nox <112299633+coco-nox@users.noreply.github.com>
2026-09-07 14:48:30 +1000

    Update

--- obj/Debug/net10.0/TimeManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/TimeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766c6fc432440566e1898a2fec4d3db84e557d0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f37e5c9a628f0061b56000a4851b789981cd2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
